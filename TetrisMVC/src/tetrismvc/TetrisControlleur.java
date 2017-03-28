8d5d0c222aa8af730f8f50ad17327d9fbc403da4
Axel Pittore <axel.pittore@etu.univ-lyon1.fr>
2017-03-28 12:38:58 +0200

    commit private (pour merge)

---
+++ TetrisMVC/src/tetrismvc/TetrisControlleur.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tetrismvc;
+
+/**
+ *
+ * @author Axel
+ */
+public class TetrisControlleur {
+    
+    public void chutePieceCourante(Grille grille){
+        grille.chutePieceCourante();
+    }
+    
+}
